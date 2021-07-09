a67cf8eb7db394a7edca686017122b1ac5fe2e98
nafiz1307 <55802844+nafiz1307@users.noreply.github.com>
2021-07-10 02:18:38 +0600

    Product validation implemented

--- controllers/product.controllers.js
+++ controllers/product.controllers.js
@@ -1,32 +1,50 @@
-const formidable = require('formidable')
-const _ = require("lodash")
-const { errorHandler } = require('../helpers/dbErrorHandler')
-const Product = require("../models/product.models")
-const fs = require('fs');
+const formidable = require("formidable");
+const _ = require("lodash");
+const { errorHandler } = require("../helpers/dbErrorHandler");
+const Product = require("../models/product.models");
+const fs = require("fs");
 
-exports.create = (req,res)=>{
-    let form = new formidable.IncomingForm()
-    form.keepExtensions = true
-    form.parse(req,(err,fields,files)=>{
-        if(err){
-            return res.status(400).json({
-                error: 'Image could not be uploaded'
-            })
-        }
-        let product = new Product(fields)
+exports.create = (req, res) => {
+  let form = new formidable.IncomingForm();
+  form.keepExtensions = true;
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      return res.status(400).json({
+        error: "Image could not be uploaded",
+      });
+    }
 
-        if(files.photo){
-            product.photo.data = fs.readFileSync(files.photo.path)
-            product.photo.contentType = files.photo.type
-        }
+    //check for all fields
+    const { name, description, price, category, quantity, shipping } = fields;
 
-        product.save((err,result)=>{
-            if(err){
-                return res.status.json({
-                    error : errorHandler(err)
-                });
-            }
-            res.json(result);
-        })
-    })
-}
+    if (!name || !description || !price || !category || !quantity || !shipping) {
+      return res.status(400).json({
+        error: "All fields are required",
+      });
+    }
+
+    let product = new Product(fields);
+    //1kb=1000
+    //1mb=1000000
+
+    if (files.photo) {
+      // console.log("FILES PHOTO: ",files.photo);
+      if (files.photo.size > 1000000) {
+        return res.status(400).json({
+          error: "Image should be less than 1mb",
+        });
+      }
+      product.photo.data = fs.readFileSync(files.photo.path);
+      product.photo.contentType = files.photo.type;
+    }
+
+    product.save((err, result) => {
+      if (err) {
+        return res.status.json({
+          error: errorHandler(err),
+        });
+      }
+      res.json(result);
+    });
+  });
+};
