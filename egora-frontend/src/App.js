3b675494c135be1214ee915d5167ec47bde26d7b
nafiz1307 <55802844+nafiz1307@users.noreply.github.com>
2021-07-28 22:57:03 +0600

    implementing react

---
+++ egora-frontend/src/App.js
@@ -0,0 +1,5 @@
+
+const App =() => <div>Hello World</div>
+
+
+export default App;
---
+++ egora-frontend/src/Routes.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./core/Home";
+import Signin from "./user/Signin";
+import Signup from "./user/Signup";
+import Menu from "./core/Menu";
+
+const Routes = () => {
+  return (
+    <BrowserRouter>
+      <Menu />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/signin" exact component={Signin} />
+        <Route path="/signup" exact component={Signup} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+export default Routes;
